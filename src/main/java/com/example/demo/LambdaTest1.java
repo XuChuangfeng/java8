2f8dca2dca477e46bab6d9179d700e86c0fe8eae
xuchuangfeng <chuangfengxu@gmail.com>
2017-11-19 20:48:03 +0800

    20点47分 - 函数式接口定义；lambda表达式的简单使用；

---
+++ src/main/java/com/example/demo/LambdaTest1.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+/**
+ * 函数式接口：接口中仅有一个抽象方法（默认方法跟静态方法不计入）
+ * Lambda表达式是用于函数式接口的
+ * @author chuan
+ */
+public class LambdaTest1 {
+    public static void main(String[] args) {
+        // 在java8之前，只能通过以下方法,匿名内部类的方式实例化一个接口
+        LambdaInterface1 lambdaInterface11 = new LambdaInterface1() {
+            @Override
+            public void test() {
+                System.out.println("使用匿名内部类的方式!");
+            }
+        };
+        lambdaInterface11.test();
+
+        LambdaInterface1 lambdaInterface12 = () -> System.out.println("使用lambda表达式简化！");
+        lambdaInterface12.test();
+
+        LambdaInterface2 lambdaInterface21 = (String str) -> System.out.println("使用Lambda表达式简化！");
+        lambdaInterface21.test("");
+        lambdaInterface21.defaultTest();
+    }
+}
+@FunctionalInterface
+interface LambdaInterface1 {
+    void test();
+}
+@FunctionalInterface
+interface LambdaInterface2 {
+    void test(String str);
+    default void defaultTest() {
+        System.out.println("default function in functional interface!");
+    }
+}
